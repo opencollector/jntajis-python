85e6c33075503c3bdfc32bde11ce8e33cff00eb5
Moriyoshi Koizumi <moriyoshi.koizumi@open-c.jp>
2021-08-23 20:46:45 +0900

    Use setuptools-scm.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, Extension
 
 setup(
+    use_scm_version={
+        "tag_regex": r"^(?P<version>[vV]?\d+(?:\.\d+){0,2}[^\+]*)(?:\+.*)?$"
+    },
     ext_modules=[
         Extension("jntajis._jntajis", ["src/jntajis/_jntajis.pyx"]),
     ],
