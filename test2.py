1c913971519f614cdeb26e66094e3a58fa1c823c
Moriyoshi Koizumi <moriyoshi.koizumi@open-c.jp>
2021-08-29 21:21:54 +0900

    Unintended file

--- test2.py
+++
@@ -1,4 +0,0 @@
-import jntajis
-
-candidates = [jntajis.jnta_shrink_translit(c) for c in jntajis.mj_shrink_candidates("器", 0b1111)]
-print(ord(candidates[0]), ord(candidates[1]), candidates[0] == candidates[1])
